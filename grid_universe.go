a57dc3452c7e1edcb2520d56abaf1588f6e71001
Javier Cervantes <1.27201@gmail.com>
2014-12-21 21:30:32 +0100

    Adding tests and docs to grid universe

--- grid_universe.go
+++ grid_universe.go
@@ -5,12 +5,15 @@ import (
 	"math/rand"
 )
 
+// GridUniverse contains a representation of a rectangular universe
 type GridUniverse struct {
 	height int
 	width  int
 	grid   [][]bool
 }
 
+// NewGridUniverse creates a Universe of the specified size.
+// It also generates an initial randome state with 25% alive cells.
 func NewGridUniverse(w, h int) Universe {
 	grid := createGrid(w, h)
 	for i := 0; i < (w * h / 4); i++ {
@@ -23,7 +26,9 @@ func NewGridUniverse(w, h int) Universe {
 	}
 }
 
-func (u GridUniverse) Apply(r Rules) Universe {
+// Apply uses the rules to generate a new generation
+// using current universe grid.
+func (u GridUniverse) Apply(rules Rules) Universe {
 	future := createGrid(u.width, u.height)
 
 	for y := 0; y < u.height; y++ {
@@ -32,15 +37,16 @@ func (u GridUniverse) Apply(r Rules) Universe {
 				alive:      u.grid[y][x],
 				neighbours: u.aliveNeighbours(x, y),
 			}
-			future[y][x] = r(cell)
+			future[y][x] = rules(cell)
 		}
 	}
 
 	u.grid = future
-
 	return u
 }
 
+// String represents the Universe as a string.
+// Alive cells are marked with an '*'.
 func (u GridUniverse) String() string {
 	var buf bytes.Buffer
 	for y := 0; y < u.height; y++ {
@@ -59,7 +65,7 @@ func (u GridUniverse) String() string {
 func (u GridUniverse) aliveNeighbours(x, y int) (n int) {
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
-			if (j != 0 || i != 0) && u.alive(x+i, y+j) {
+			if (j != 0 || i != 0) && u.wrap(x+i, y+j) {
 				n++
 			}
 		}
@@ -67,7 +73,7 @@ func (u GridUniverse) aliveNeighbours(x, y int) (n int) {
 	return
 }
 
-func (u GridUniverse) alive(x, y int) bool {
+func (u GridUniverse) wrap(x, y int) bool {
 	wrapped_x := (x + u.width) % u.width
 	wrapped_y := (y + u.height) % u.height
 
---
+++ grid_universe_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSeed(t *testing.T) {
+	u := NewGridUniverse(10, 10)
+
+	if !strings.Contains(u.String(), "*") {
+		t.Error("Universe should have some alive cells")
+	}
+}
+
+func TestApply(t *testing.T) {
+	u := NewGridUniverse(10, 10)
+
+	nu := u.Apply(func(c Cell) bool {
+		return false
+	})
+
+	if strings.Contains(nu.String(), "*") {
+		t.Error("Universe should have killed all cells")
+	}
+}
+
+func TestWrap(t *testing.T) {
+	var tests = []struct {
+		x1 int
+		y1 int
+		x2 int
+		y2 int
+	}{
+		{0, 0, 10, 0},
+		{1, 0, 11, 0},
+		{0, 2, 0, 12},
+		{0, 4, 0, 14},
+		{5, 5, 15, 15},
+	}
+
+	u := NewGridUniverse(10, 10).(GridUniverse)
+
+	for _, c := range tests {
+
+		if u.wrap(c.x1, c.y1) != u.wrap(c.x2, c.y2) {
+			t.Error(
+				"Universe is not wrapped correctly:",
+				c.x1, ",", c.y1,
+				"should equal:",
+				c.x2, ",", c.y2,
+			)
+		}
+	}
+}
